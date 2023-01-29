e228c44d0abc238ddfa27a7b0078c8599bcfb5ad
Sep <esparago.joseph0002@gmail.com>
2023-01-30 00:03:01 +0800

    updated files

--- index.js
+++ index.js
@@ -18,5 +18,32 @@ const handleLoveClick = () =>{
 
     nextGroup.dataset.status = "becoming-active-from-before";
 
-    activeIndex = nextIndex;
+    setTimeout(() =>{
+        nextGroup.dataset.status = "active"
+        activeIndex = nextIndex;
+    })
+    
+}
+
+const handleHateClick = () =>{
+    //Bump active index
+
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    //active group becomes after
+
+    currentGroup.dataset.status = "before";
+
+    //next group becomes active
+
+    nextGroup.dataset.status = "becoming-active-from-after";
+
+    setTimeout(() =>{
+        nextGroup.dataset.status = "active"
+        activeIndex = nextIndex;
+    })
+    
 }
